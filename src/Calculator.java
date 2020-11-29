4831d6d2062e0702748bce8bf1e903415591f3ea
sDriskell <Driskell.Shane.L@gmail.com>
2020-11-29 08:14:09 -0500

    Added simple do/while loops for user input

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,10 +3,7 @@
 import java.util.Scanner;
 
 public class Calculator {
-	private static String firstNumber;
-	private static String secondNumber;
-	private static String arth;
-	private static double total;
+
 	public static boolean isNumeric(String strNum) { // Creator of function: baeldung
 		if (strNum == null) {
 			return false;
@@ -19,40 +16,48 @@ public class Calculator {
 		return true;
 	}	
 	public static void main(String[] args) {
-
 		Scanner scan = new Scanner(System.in);
-
+		String  firstNumber;
+		String  secondNumber;
+		String  arth;
+		char    keepGoing;
+		double  total = 0;
 		do {
-			System.out.print("Enter 1st number: ");
-			firstNumber = scan.nextLine();
-			System.out.print("Enter 2nd number: ");
-			secondNumber = scan.nextLine();
-		} while (isNumeric(firstNumber) == false &&
-				isNumeric(secondNumber) == false);
-
-		System.out.println("+, -, *, or / : ");
-		arth = scan.nextLine();
-		if (arth.contains("+")) {
-			total = Double.parseDouble(firstNumber) + Double.parseDouble(secondNumber);
-		}
-		else if (arth.contains("-")) {
-			total = Double.parseDouble(firstNumber) - Double.parseDouble(secondNumber);
-		}
-		else if (arth.contains("*")) {
-			total = Double.parseDouble(firstNumber) * Double.parseDouble(secondNumber);
-		}
-		else if (arth.contains("/")) {
-			total = Double.parseDouble(firstNumber) / Double.parseDouble(secondNumber);
-		}
-		else {
-			System.out.println("Incorrect symbol.");
-		}
-		
-		try {
-			System.out.println("Total: " + total);
-		} catch (Exception e) {
-			System.out.println("One or more component not valid.");
-		}
+			do {
+				System.out.print("Enter 1st number: ");
+				firstNumber = scan.nextLine();
+				System.out.print("Enter 2nd number: ");
+				secondNumber = scan.nextLine();
+			} while (isNumeric(firstNumber) == false &&
+					isNumeric(secondNumber) == false);
+	
+			System.out.println("+, -, *, or / : ");
+			arth = scan.nextLine();
+			if (arth.contains("+")) {
+				total = Double.parseDouble(firstNumber) + Double.parseDouble(secondNumber);
+			}
+			else if (arth.contains("-")) {
+				total = Double.parseDouble(firstNumber) - Double.parseDouble(secondNumber);
+			}
+			else if (arth.contains("*")) {
+				total = Double.parseDouble(firstNumber) * Double.parseDouble(secondNumber);
+			}
+			else if (arth.contains("/")) {
+				total = Double.parseDouble(firstNumber) / Double.parseDouble(secondNumber);
+			}
+			else {
+				System.out.println("Incorrect symbol.");
+			}
+			
+			try {
+				System.out.println("Total: " + total);
+			} catch (Exception e) {
+				System.out.println("One or more component not valid.");
+			}
+			
+			System.out.print("Another computation ([y]es)?: ");
+			keepGoing = scan.nextLine().toLowerCase().charAt(0);
+		} while (keepGoing == 'y');
 		scan.close();
 	}
 }
